6609f6e9dad817d9555e6f7f966504bc02aca783
shakti97 <shaktisingh230797@gmail.com>
2019-09-29 12:58:54 +0530

    Added Search Specific Repo, Refactor Code for getting issues

--- src/components/issues/issueContainer.js
+++ src/components/issues/issueContainer.js
@@ -5,36 +5,47 @@ import Issue from "./Issue";
 import Spinner from "../reusable/Spinner/spinner";
 
 class IssueContainer extends React.Component {
-  state = { issues: [], loading: false };
+  state = { issues: [], loading: false, error: "" };
 
   async componentDidMount() {
     this.setState(current => {
       return { ...current, loading: true };
     });
-    let issues = await getIssues(this.props.project);
-
-    this.setState(current => {
-      return { ...current, loading: false, issues };
-    });
+    this.fetchIssues();
   }
   async componentDidUpdate(prevProps) {
     if (!isEqual(prevProps, this.props)) {
       this.setState(current => {
         return { ...current, loading: true };
       });
-      let issues = await getIssues(this.props.project);
-
-      this.setState(current => {
-        return { ...current, loading: false, issues };
-      });
+      this.fetchIssues();
     }
   }
+  fetchIssues = async () => {
+    try {
+      let issues = await getIssues(this.props.project);
+      this.setState(current => {
+        return { ...current, loading: false, issues, error: "" };
+      });
+    } catch (error) {
+      this.setState(current => {
+        return {
+          ...current,
+          loading: false,
+          error: "Not Supported Yet, Work in Progress See You soon 🙂"
+        };
+      });
+    }
+  };
   render() {
+    const { error, loading, issues } = this.state;
     return (
       <div>
-        {this.state.loading ? (
+        {loading ? (
           <Spinner />
-        ) : this.state.issues.length === 0 ? (
+        ) : error !== "" ? (
+          error
+        ) : issues.length === 0 ? (
           "No Good-First issues Found 😭"
         ) : (
           this.state.issues.map(issue => <Issue issue={issue} />)
--- src/components/projects/projectsContainer.js
+++ src/components/projects/projectsContainer.js
@@ -7,6 +7,19 @@ class ProjectsContainer extends Component {
   render() {
     return (
       <>
+        <div id="wrap">
+          <form autoComplete="on" onSubmit={(event)=>{event.preventDefault();this.props.selectProject(this.searchedRepo.value)}}>
+            <input
+              id="search"
+              name="search"
+              ref={value=> this.searchedRepo=value}
+              type="text"
+              className='search-box'
+              placeholder="What're you looking for ?"
+            />
+            <input id="search_submit" value="Rechercher" type="submit" className='search-submit'/>
+          </form>
+        </div>
         <div className="button-container">
           {projects &&
             projects.map(project => (
